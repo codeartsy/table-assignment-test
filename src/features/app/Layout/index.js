b6efdeb08be95f79393a7aae697b47e7415cd0dc
codeartsy <codeartsy@gmail.com>
2023-08-29 03:37:16 +0530

    changed menu

--- src/features/app/Layout/index.js
+++ src/features/app/Layout/index.js
@@ -1,25 +1,70 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import './Layout.css';
 
 
 const Layout=({ children })=>{
+  const [showNavbar, setShowNavbar] = React.useState(false);
+
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar);
+  };
+
+  const MobileMenuIcon = () => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="52"
+      height="24"
+      viewBox="0 0 52 24"
+    >
+      <g id="Group_9" data-name="Group 9" transform="translate(-294 -47)">
+        <rect
+          id="Rectangle_3"
+          data-name="Rectangle 3"
+          width="42"
+          height="4"
+          rx="2"
+          transform="translate(304 47)"
+          fill="#fff"
+        />
+        <rect
+          id="Rectangle_5"
+          data-name="Rectangle 5"
+          width="42"
+          height="4"
+          rx="2"
+          transform="translate(304 67)"
+          fill="#fff"
+        />
+        <rect
+          id="Rectangle_4"
+          data-name="Rectangle 4"
+          width="52"
+          height="4"
+          rx="2"
+          transform="translate(294 57)"
+          fill="#fff"
+        />
+      </g>
+    </svg>
+  );
+
   return(
     <div>
-      <div class="navbar">
-        <div class="navbar-container container">
-          <div class="hamburger-lines">
-            <span class="line line1"></span>
-            <span class="line line2"></span>
-            <span class="line line3"></span>
+      <nav className="navbar">
+        <div className="menu-container">
+          <div className="menu-icon" onClick={handleShowNavbar}>
+            <MobileMenuIcon />
           </div>
-          <ul class="menu-items">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/appsettings">App Settings</Link></li>
-          </ul>
-          <h1 class="logo">Basic Table</h1>
+          <div className={`nav-elements  ${showNavbar && "active-link"}`}>
+            <ul>
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
+      </nav>
       <div className="body-container">
         { children }
       </div>
